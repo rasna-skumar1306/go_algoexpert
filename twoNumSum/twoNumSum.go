a45ff1dec9765e2e483c68691bfb9141a6c01d21
rasna-skumar1306 <rasna5202@gmail.com>
2020-11-21 20:54:26 +0530

    twonumsum problem

---
+++ twoNumSum/twoNumSum.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+//TwoNumberSum is the function to print the the numbers that sum up to give the target sum
+func TwoNumberSum(array []int, target int) []int {
+	numsMap := map[int]bool{}
+	for _, num := range array {
+		match := target - num
+		if _, found := numsMap[match]; found {
+			return []int{match, num}
+		}
+		numsMap[num] = true
+	}
+	return []int{}
+}
+
+func main() {
+	arr := []int{3, 5, -4, 8, 11, 1, -1, 6}
+	target := 10
+	fmt.Print(TwoNumberSum(arr, target))
+}
